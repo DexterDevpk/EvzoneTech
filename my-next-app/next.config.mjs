8966730af70138a1bdd24bdf4fde201eb5324a9b
DexterDev <ymksheikh@gmail.com>
2025-03-13 22:59:58 +0500

    Update next.config.mjs

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/EvzoneTech', // Required for GitHub Pages
+};
 
 export default nextConfig;
